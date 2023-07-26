e53e9908ed803ced7bb0fd741d6fc536c25328f6
Harinder <harrythind41@gmail.com>
2023-07-25 20:00:28 -0400

    feat: adding login

--- src/components/login-form/LoginForm.js
+++ src/components/login-form/LoginForm.js
@@ -1,10 +1,60 @@
+import './LoginForm.css';
+import {useState} from "react";
+import {useSelector} from "react-redux";
+import {selectFlow} from "../../core/redux-store/slices/flowSlice";
+import AdminService from "../../core/services/AdminService";
+import UserService from "../../core/services/UserService";
 
-const LoginForm =({props}) => {
+const LoginForm =() => {
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [isUsernameMissing, setIsUsernameMissing] = useState(false);
+    const [isPasswordMissing, setIsPasswordMissing] = useState(false);
+    const [showError, setShowError] = useState(false);
+    const flow = useSelector(selectFlow);
+
+    const handleNameChange = (event) => {
+        setUsername(event.target.value);
+        setIsUsernameMissing(false);
+    }
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+        setIsPasswordMissing(false);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if(username === '' || password === ''){
+            if(username === ''){setIsUsernameMissing(true)}
+            if(password === ''){setIsPasswordMissing(true)}
+        }
+        else if(flow === 'admin'){
+            AdminService.verifyLogin({username: username, password: password})
+        }else if(flow === 'user'){
+            UserService.verifyLogin({username: username, password: password})
+        }
+    }
+
     return (
-        <div>
-
+        <div className='login-form'>
+            <form onSubmit={handleSubmit}>
+                <div className='form-row'>
+                    <label htmlFor="name" style={{display:'block'}}>Username</label>
+                    <input className='form-input' type="text" id="name" name="name" onChange={handleNameChange}/>
+                    {isUsernameMissing && <div className='error'>Username required</div>}
+                </div>
+                <div className='form-row'>
+                    <label htmlFor="password" style={{display: 'block'}}>Password</label>
+                    <input className='form-input' type="password" id="password" name="password" onChange={handlePasswordChange}/>
+                    {isPasswordMissing && <div className='error'>Password required</div>}
+                </div>
+                <button className='submit-button' type="submit">Submit</button>
+            </form>
         </div>
     )
-}
 
+}
 export default LoginForm;
--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,20 +1,15 @@
-import UserLoginButton from "../../components/login-buttons/user-login-button/UserLoginButton";
-import AdminLoginButton from "../../components/login-buttons/admin-login-button/AdminLoginButton";
-import { useSelector } from "react-redux";
-import { selectFlow } from "../../core/redux-store/slices/flowSlice";
 import './HomePage.css';
 import LoginButtons from "../../components/login-buttons/LoginButtons";
+import LoginForm from "../../components/login-form/LoginForm";
 
 const HomePage = () => {
-
-    const flow = useSelector(selectFlow);
 
     return(
         <div className='home-page-wrapper'>
             <div className='form-wrapper'>
                 <LoginButtons />
-                <div className='form-card'>
-
+                <div className='login-form'>
+                    <LoginForm />
                 </div>
             </div>
 
