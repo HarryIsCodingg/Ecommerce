880a837da5c0bff42bddb7906bef48ef81beaad1
Harinder Singh <93735475+HarryIsCodingg@users.noreply.github.com>
2023-11-10 00:04:20 -0500

    Feat/backend url (#15)
    * fix: change the url of backend rest
    
    * fix: change the url of backend rest
    
    * adding debug mode

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -4,10 +4,17 @@ import LoginForm from "../../components/login-form/LoginForm";
 import {useSelector} from "react-redux";
 import {selectCurrentUser} from "../../core/redux-store/slices/userSlice";
 import BasketModal from "../../components/basket-modal/BasketModal";
+import {useState} from "react";
+import DebugMode from "../../components/debug-mode/DebugMode";
 
 const HomePage = () => {
 
     const currentUser = useSelector(selectCurrentUser);
+    const [debugMode, setDebugMode] = useState(false);
+
+    const handleDebug = () => {
+        setDebugMode(prevState => !prevState);
+    }
 
     return(
         <div className='home-page-wrapper'>
@@ -18,6 +25,15 @@ const HomePage = () => {
                     <LoginForm />
                 </div>
             </div>
+            <div onClick={handleDebug} className='debug-mode'>
+                <div style={{background: 'orange', padding: '8px', borderTopLeftRadius: '8px', borderTopRightRadius: '8px'}}>Debug</div>
+                {debugMode &&
+                    <>
+                        <DebugMode />
+                    </>
+                }
+            </div>
+
         </div>
     )
 }
