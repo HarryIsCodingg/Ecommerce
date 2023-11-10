2c44cd8f1c09be3250c310bcf8dd204313f83b8c
Harinder <harrythind41@gmail.com>
2023-11-09 23:39:55 -0500

    adding debug mode

---
+++ src/components/debug-mode/DebugMode.js
@@ -0,0 +1,16 @@
+const DebugMode = () => {
+    return (
+        <div style={{padding: '20px'}}>
+            <div className='column-1' style={{borderBottom: '1px solid gainsboro', paddingBottom: '16px', marginTop: '16px'}}>
+                <h3>username: harinder</h3>
+                <h3>password: admin</h3>
+            </div>
+            <div className='column-1' style={{paddingTop: '16px'}}>
+                <h3>username: harinder</h3>
+                <h3>password: user</h3>
+            </div>
+        </div>
+    )
+}
+
+export default DebugMode;
